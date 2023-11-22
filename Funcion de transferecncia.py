e77b8946688130fb6c718741c281fdb08309498a
Sebastian1029 <chebis1029@gmail.com>
2023-11-22 09:03:53 -0500

    Revert "Lo dañé"
    This reverts commit cd63cdfb4860b1f3b047332eba11d1282e378d55.

--- Funcion de transferecncia.py
+++ Funcion de transferecncia.py
@@ -2,4 +2,10 @@ from matplotlib import pyplot as plt
 import numpy as np
 wc=2
 init=0.1
-endi
+ending=100
+Npasos=int(input("Ingrese pasos"))
+w=np.linspace(init,ending,Npasos)
+magH=20*np.log10((w/wc)/((w/wc)**2+1)**(1/2))
+plt.plot(w,magH)
+plt.scatter([wc],[-3.01])
+plt.show()
