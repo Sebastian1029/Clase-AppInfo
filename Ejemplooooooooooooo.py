6556fc5327a6ef78dc2d3295506f36f08bc1d129
Sebastian1029 <chebis1029@gmail.com>
2023-11-22 09:29:16 -0500

    Revert "Update Ejemplooooooooooooo.py"
    This reverts commit 83533da0acfc068b5d39b0d5b36d264a89f8bd31.

--- Ejemplooooooooooooo.py
+++ Ejemplooooooooooooo.py
@@ -2,6 +2,14 @@ from matplotlib import pyplot
 import math
 
 
+def Magnitud(w,wc):
+    return 20*math.log10(1/(1+(w/wc)**2)**(1/2))
+
+
+w=[i*0.01 for i in range(1,1001)]
+
+
+H=[Magnitud(w_i,5) for w_i in w]
 
 pyplot.plot(w,H)
 pyplot.show()
