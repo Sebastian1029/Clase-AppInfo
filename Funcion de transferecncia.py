02464d78a1061b4cc547c955e8dcad3d7ff6d2ce
Sebastian1029 <chebis1029@gmail.com>
2023-11-22 09:04:56 -0500

    Daño 2
    Se daño otra vez

--- Funcion de transferecncia.py
+++ Funcion de transferecncia.py
@@ -3,9 +3,4 @@ import numpy as np
 wc=2
 init=0.1
 ending=100
-Npasos=int(input("Ingrese pasos"))
-w=np.linspace(init,ending,Npasos)
-magH=20*np.log10((w/wc)/((w/wc)**2+1)**(1/2))
-plt.plot(w,magH)
-plt.scatter([wc],[-3.01])
-plt.show()
+
